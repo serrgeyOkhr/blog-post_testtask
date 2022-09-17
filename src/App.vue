b078d9b171ab34094a154b65b17c005a9a69f778
Sergey <owlbert@null.net>
2022-09-17 23:17:34 +0700

    Стили для панели навигации

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Posts</router-link> |
-    <router-link to="/users">Users</router-link>
+  <nav class="header_navigation">
+    <router-link to="/" class="link">Посты</router-link>
+    <router-link to="/users" class="link">Пользователи</router-link>
   </nav>
   <router-view />
 </template>
@@ -21,5 +21,27 @@ body {
 
 img {
   max-width: 100%;
+}
+.header_navigation {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.link {
+  text-decoration: none;
+  color: black;
+  padding: 10px 15px;
+  border-bottom: 1px solid;
+  margin: 0 5px;
+}
+.link:hover {
+  background-color: rgb(214, 214, 214);
+}
+.router-link-active {
+  background: #000;
+  color: #fff;
+}
+.router-link-active:hover {
+  background: #000;
 }
 </style>
