e795a4bbd017025c65bbaa8e8b1caf5fa2b38b24
Sergey <owlbert@null.net>
2022-09-17 19:24:21 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  </nav> -->
   <router-view />
 </template>
 
@@ -15,16 +15,11 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+body {
+  box-sizing: border-box;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+img {
+  max-width: 100%;
 }
 </style>
